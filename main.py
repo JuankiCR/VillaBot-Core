f79f150b2dec9171e730a841ba38c57ecb721bed
JuankiCR <59723157+JuankiCR@users.noreply.github.com>
2023-03-25 12:46:39 -0600

    initial example file

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import discord
+
+token = os.environ['TOKEN']
+symbol = '!'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith(symbol + 'hi'):
+    await message.channel.send(f'Hello, {message.author}! \nHere {client.user}, what can i do for you?')
+
+client.run(token)
