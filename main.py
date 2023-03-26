868803cd8d44045239fd09a2d415dca36ed63289
MrVullcan <Manuelvilla7052@gmail.com>
2023-03-25 23:58:17 -0600

    Primera Actualizacion

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 from decouple import config
 import discord
+from discord.ext import commands
+import datetime
 
-symbol = '!'
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents = intents)
+bot = commands.Bot(command_prefix = '!', intents = intents)
 
-@client.event
+#Events
+@bot.event
 async def on_ready():
-  print(f'We have logged in as {client.user}')
+  await bot.change_presence(activity = discord.Streaming(name = "Gimme Money", url = "https://www.twitch.tv/juankicr"))
+  print(f'We have logged in as {bot.user}')
+  print(f"The Bot Is Loaded And Ready")
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+#Hi Message  
+@bot.command()
+async def Hi(cxt):  
+  await cxt.send(f'Hello! {cxt.author}! \nThis is your helper bot {bot.user}, what can i do for you?')
 
-  if message.content.startswith(symbol + 'hi'):
-    await message.channel.send(f'Hello, {message.author}! \nHere {client.user}, what can i do for you?')
+#Test
+@bot.command()
+async def Ping(ctx):
+  await ctx.send('Pong')
+    
+#Stats Message
+@bot.command()
+async def Stats(ctx):
+    embed = discord.Embed(
+      title = "SERVER_INFO",
+      description = f'! + hi: Command #1 \n ! + Ping: Command #2', 
+      timestamp = datetime.datetime.utcnow(), 
+      color = discord.Color.purple())
+    embed.add_field(name = "Server Owner", value = "@JuankiCR")
+    embed.add_field(name = "Server Region", value = "Mexico")
+    embed.set_thumbnail(url = "https://i.pinimg.com/originals/52/fd/67/52fd6714a21542abdfc167bc67969c8a.jpg")
+    await ctx.send(embed = embed)
 
-client.run(config('T_TOKEN'))
+bot.run(config('M_TOKEN'))
+
+
+
